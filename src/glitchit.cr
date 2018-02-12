16a0bfe38f4d84eec440d7956e40ec439e6e9f89
Cristian Guerrero <cristian@s9.cl>
2018-02-12 00:21:31 -0800

    Add initial glitcher

--- src/glitchit.cr
+++ src/glitchit.cr
@@ -61,7 +61,58 @@ cli = Commander::Command.new do |cmd|
     finish = options.int["finish"]      # => 1
     arguments                           # => Array(String)
     abort cmd.help if in_file.blank?
+
+    Glitcher.new(in_file, out_file, bytes, times, start, finish).glitchit
   end
 end
 
 Commander.run(cli, ARGV)
+
+class Glitcher
+  def initialize(@input_file : String,
+                 @output_file : String = "glitched.jpg",
+                 @bytes : (Int32 | Int64) = 20,
+                 @times : (Int32 | Int64) = 5,
+                 @start : (Int32 | Int64) = 0,
+                 @finish : (Int32 | Int64) = 1)
+    abort "input file must be jpg" unless input_file.ends_with?("jpg")
+    @total_bytes = Int32.new(File.size(@input_file))
+    @header_size = Int32.new(2000)
+  end
+
+  def glitchit
+    output = File.new(@output_file, "w")
+
+    File.open(@input_file) do |file|
+      count = 0
+
+      bytes_to_shift = generate_array_of_safe_bytes
+
+      file.each_byte do |byte|
+        while count < @header_size
+          count += 1
+          next
+        end
+
+        if bytes_to_shift.includes?(count)
+          output.write_byte(byte.succ)
+        else
+          output.write_byte(byte)
+        end
+
+        count += 1
+      end
+    end
+  end
+
+  # Generate an array of bytes outside of header to glitch
+  # TODO: Find a better way to determine if safe bytes (we are guessing after 2000 atm)
+  def generate_array_of_safe_bytes
+    bytes_to_shift = Array(Int32).new
+    @times.times do
+      bytes_to_shift << rand(@header_size...@total_bytes)
+      bytes_to_shift.sort!
+    end
+    bytes_to_shift
+  end
+end
